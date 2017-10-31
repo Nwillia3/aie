302e3a6f81ccaa3a6eac0b385e7b235b63272d84
Neil Williams <neil.williams227@gmail.com>
2017-10-30 22:07:26 -0400

    navbar integration

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,20 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.precompile += %w( modernizr.js )
+Rails.application.config.assets.precompile += %w( jquery-2.1.1.js )
+Rails.application.config.assets.precompile += %w( main.js )
+
+
+Rails.application.config.assets.precompile += %w( get_involved.scss )
+Rails.application.config.assets.precompile += %w( home.scss )
+Rails.application.config.assets.precompile += %w( projects.scss )
+Rails.application.config.assets.precompile += %w( reset.scss )
+Rails.application.config.assets.precompile += %w( style.scss )
+Rails.application.config.assets.precompile += %w( why_aie.scss )
+Rails.application.config.assets.precompile += %w( about.scss )
+Rails.application.config.assets.precompile += %w( our_approach.scss )
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
